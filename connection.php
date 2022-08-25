33c27b5bbff36dc492cd5eec59addf566f0e9708
Disha Mahajan <81662188+DishaMahajan22@users.noreply.github.com>
2022-08-24 17:50:38 -0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,29 @@
+<?php
+if(!isset($_POST['name']) ||
+
+!isset($_POST['message']) ||
+
+!isset($_POST['email'])) {
+
+die('We are sorry to proceed your request due to error within form entries');   
+
+}
+$name = $_POST['name'];
+$email = $_POST['email'];
+$msg = $_POST['message'];
+$email_to = "disham2004@gmail.com";
+$email_sub .= "Name: ". $name. " From: ". $email;
+
+$email_message .= "First Name: ".($first_name)."\n";
+
+$email_message .= "Last Name: ".($last_name)."\n";
+
+$email_message .= "Email: ".($email_from)."\n";
+
+$email_message .= "Telephone: ".($telephone)."\n";
+
+$email_message .= "Comments: "($comments)."\n";
+$headers = 'From: '.$email."\r\n";
+@mail($email_to, $email_sub, $msg, $headers);
+?>
+Thank you for your response. I will reach out to you as soon as possible.
